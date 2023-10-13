e4fff4aecd79d65c79624aa7cb94f54ec3657764
szt2palpat <144673180+szt2palpat@users.noreply.github.com>
2023-10-13 08:26:40 +0200

    dun

---
+++ Object2/js.js
@@ -0,0 +1,11 @@
+function isEMpty(obj){
+    let i=0
+    for (const key in obj) {
+        i++;
+            
+        }
+        i===0;
+       
+    }
+    console.log(isEMpty())
+    console.log(isEMpty({name: "John"}))
